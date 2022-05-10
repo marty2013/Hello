f27cc88b1d4085ce8c3ae1682ea7d825a4d2ff3e
Martin Gay <martin@systrading.com>
2022-05-10 14:51:26 +1000

    Added the random guess game example function

--- main.jl
+++ main.jl
@@ -13,3 +13,29 @@ p = foo( cars, "USA")
 
 p |> save("foo.png")
 
+# the following can be run separately - it is a random number guessing game
+
+function play_number_guess()
+
+  total_number = 5
+
+  target_number = rand(1:total_number)
+  guess = 0
+
+  while guess != target_number
+    print("Please guess a number between 1 and ", total_number, ": ")
+    guess = parse(Int64, readline())
+
+    if guess != target_number
+      println("Guess again !!")
+    end
+  end
+  println("You guessed correctly!")
+end
+
+
+
+
+
+
+
